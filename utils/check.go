5f8230535b802f393125c5b34ad1ff810d4920b1
k.otdelnov <k.otdelnov@gispuzzle.ru>
2020-08-05 15:37:50 +0200

    функция FailIfNil

--- utils/check.go
+++ utils/check.go
@@ -1,11 +1,18 @@
 package utils
 
 import (
+	"fmt"
 	"log"
 )
 
 func FailIfError(err error) {
 	if err != nil {
 		log.Fatalf("%s", err)
+	}
+}
+
+func FailIfNil(v interface{}, msg string) {
+	if v == nil {
+		log.Fatalf(fmt.Sprintf("variable is nil: %s", msg))
 	}
 }
