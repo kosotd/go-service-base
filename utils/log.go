f5f4dd7933537eac9593d8f8b37753a11f8c3e99
k.otdelnov <k.otdelnov@gispuzzle.ru>
2020-08-05 15:17:13 +0200

    формат вывода ошибок

--- utils/log.go
+++ utils/log.go
@@ -5,6 +5,7 @@ import (
 	"github.com/pkg/errors"
 	"log"
 	"net/http"
+	"time"
 )
 
 var ll = 0
@@ -37,19 +38,19 @@ func LogTrace(format string, v ...interface{}) {
 	}
 }
 
-func LogAndSetStatus(w http.ResponseWriter, code int, err error) {
+func LogErrorAndSetStatus(w http.ResponseWriter, code int, err error) {
 	LogError("%v", err)
-	http.Error(w, fmt.Sprintf("%v", errors.Cause(err)), code)
+	http.Error(w, fmt.Sprintf("%s %v", time.Now().Format("2006/01/02 15:04:05"), errors.Cause(err)), code)
 }
 
 func LogAndSetStatusIfError(w http.ResponseWriter, code int, err error) {
 	if err != nil {
-		LogAndSetStatus(w, code, err)
+		LogErrorAndSetStatus(w, code, err)
 	}
 }
 
 func LogAndSetStatusIfRecover(w http.ResponseWriter, code int) {
 	if r := recover(); r != nil {
-		LogAndSetStatus(w, code, errors.New(fmt.Sprint(r)))
+		LogErrorAndSetStatus(w, code, errors.New(fmt.Sprint(r)))
 	}
 }
